844e946341a81f50ff1074295db1d62d5138d245
Brian Kim <brian.kyounghoon.kim@gmail.com>
2026-06-29 14:10:10 -0400

    reponsiveness WIP

--- packages/frontend/src/components/UI/components/Loader/Loader.tsx
+++ packages/frontend/src/components/UI/components/Loader/Loader.tsx
@@ -8,7 +8,7 @@ interface LoaderProps {
 
 const Loader: FC<LoaderProps> = ({ isLoading }) => {
     return (
-        <LoaderStyleContainer initial={{ opacity: 1 }} animate={{ opacity: isLoading ? 0 : 1 }}>
+        <LoaderStyleContainer initial={{ opacity: 1 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: "easeInOut" }}>
             <p>Server waking up</p>
         </LoaderStyleContainer>
     )
